57caef62cfd4a21653a5914526820c113f6a1079
PierreWTH <wietrichpierre@gmail.com>
2023-05-04 15:13:17 +0200

    form add barber ok,ajout des horaires en reflexion

--- src/Controller/BarbershopController.php
+++ src/Controller/BarbershopController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Barbershop;
+use App\Form\BarbershopType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BarbershopController extends AbstractController
 {
@@ -15,4 +19,33 @@ class BarbershopController extends AbstractController
             'controller_name' => 'BarbershopController',
         ]);
     }
+
+    #[Route('/barbershop/add', name: 'add_barbershop')]
+    public function add(ManagerRegistry $doctrine, Barbershop $barbershop = null, Request $request) : Response
+    {
+        $form = $this->createForm(BarbershopType::class, $barbershop);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+                $barbershop = $form->getData();
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($barbershop);
+                $entityManager->flush();
+
+                return $this->redirectToRout('app_barbershop');
+        }
+
+        return $this->render('barbershop/add.html.twig', [
+            'formAddBarbershop' => $form->createView()
+        ]);
+    }
+
+    #[Route('/barbershop/{id}', name: 'show_barbershop')]
+    public function show(Barbershop $barbershop) : Response
+    {
+        return $this->render('barbershop/show.html.twig', []);
+    }
 }
+
+
---
+++ src/Form/BarbershopType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Barbershop;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class BarbershopType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('adresse', TextType::class)
+            ->add('cp', NumberType::class)
+            ->add('ville', TextType::class)
+            ->add('horaires', TextareaType::class)
+            ->add('latitude', NumberType::class)
+            ->add('longitude', NumberType::class)
+            ->add('telephone', TextType::class)
+            ->add('email', TextType::class)
+            ->add('instagram', TextType::class)
+            ->add('facebook', TextType::class)
+            //->add('isValidate')
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Barbershop::class,
+        ]);
+    }
+}
