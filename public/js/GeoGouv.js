b7910b917d941feeb1719be914544b0f8d51c334
PierreWTH <wietrichpierre@gmail.com>
2023-05-07 22:47:41 +0200

    suite select2, pas fonctionnel

--- public/js/GeoGouv.js
+++ public/js/GeoGouv.js
@@ -4,14 +4,15 @@
 $(document).ready(function() 
 {
     // Selection du champs adresse et initialisation de Select2
-    $('#adresse-input').select2(
-    {
+    $('#barbershop_adresse').select2
+    ({
         // recherche se déclenche après minimum 2 caractères rentrés
         minimumInputLenght:2,
+        // Appel a l'api
         ajax: 
         {
             url: 'https://api-adresse.data.gouv.fr/search/',
-            dataType: 'json'
+            dataType: 'json',
             delay: 250,
             data : function(params)
             {
@@ -20,6 +21,34 @@ $(document).ready(function()
                     q: params.term
                 };
             },
-        }
-    }
-}
+            // Transforme les résultats de la requete AJAX en format utilisable par Select2
+            processResults: function(data) {
+                // Récuperation des données
+                var results = data.features.map(function(feature){
+                    return {
+                        id: feature.properties.id,
+                        text:feature.properties.label,
+                        city: feature.properties.city,
+                        postcode: feature.properties.postcode,
+                        adresse: feature.properties.name 
+                    };
+                });
+                return {
+                    results: results
+                };
+            },
+            cache:true
+        },
+        placeholder : 'Rechercher une adresse',
+        allowClear: true
+    })
+    // A la selection d'une adresse
+    .on('select2:select', function(e) {
+        var selectedAdresse = e.params.data;
+        
+        // Remplissage du code postal et de la ville en fonction de l'adresse choisie
+        $('#cp-input').val(selectedAdresse.postcode);
+        $('#ville-input').val(selectedAdresse.city);
+
+    });
+});
