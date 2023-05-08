eac7ee3ab274aafedf7a599ca6b03eda93b7f25a
PierreWTH <wietrichpierre@gmail.com>
2023-05-08 14:36:33 +0200

    select2 adresse fonctionnel

--- public/js/GeoGouv.js
+++ public/js/GeoGouv.js
@@ -1,54 +1,49 @@
 // AUTOCOMPLETE L'ADRESSE (en utilisant jQuery et Select2)
 
 // Une fois que la page est totalement chargée
-$(document).ready(function() 
-{
-    // Selection du champs adresse et initialisation de Select2
-    $('#barbershop_adresse').select2
-    ({
-        // recherche se déclenche après minimum 2 caractères rentrés
-        minimumInputLenght:2,
+$(document).ready(function() {
+    // Selection du champ adresse et initialisation de Select2
+    $('#barbershop_adresse').select2({
+        // recherche se déclenche après minimum 4 caractères rentrés (sinon erreur dans la console)
+        minimumInputLength: 4,
+        minimumResultsForSearch: Infinity,
         // Appel a l'api
-        ajax: 
-        {
+        ajax: {
             url: 'https://api-adresse.data.gouv.fr/search/',
             dataType: 'json',
             delay: 250,
-            data : function(params)
-            {
-                return
-                {
-                    q: params.term
+            data: function(params) {
+                return {
+                    q: decodeURI(params.term)
                 };
             },
-            // Transforme les résultats de la requete AJAX en format utilisable par Select2
+            // Transforme les résultats de la requête AJAX en format utilisable par Select2
             processResults: function(data) {
-                // Récuperation des données
-                var results = data.features.map(function(feature){
+                // Récupération des données
+                var results = data.features.map(function(feature) {
                     return {
                         id: feature.properties.id,
-                        text:feature.properties.label,
+                        text: feature.properties.label,
                         city: feature.properties.city,
                         postcode: feature.properties.postcode,
-                        adresse: feature.properties.name 
+                        adresse: feature.properties.name
                     };
                 });
+
                 return {
                     results: results
                 };
-            },
-            cache:true
-        },
-        placeholder : 'Rechercher une adresse',
-        allowClear: true
+            }
+        }
     })
-    // A la selection d'une adresse
+    // A la sélection d'une adresse
     .on('select2:select', function(e) {
         var selectedAdresse = e.params.data;
         
-        // Remplissage du code postal et de la ville en fonction de l'adresse choisie
-        $('#cp-input').val(selectedAdresse.postcode);
-        $('#ville-input').val(selectedAdresse.city);
-
+        // Remplissage des champs en fonction de l'adresse choisie
+        $('#barbershop_cp').val(selectedAdresse.postcode);
+        $('#barbershop_ville').val(selectedAdresse.city);
+        $('#barbershop_adresse').val(selectedAdresse.adresse);
+        
     });
 });
