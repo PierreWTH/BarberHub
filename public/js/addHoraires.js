563064e74600ac5b310b7510bd44b0b521aeb4f8
PierreWTH <wietrichpierre@gmail.com>
2023-05-09 18:33:24 +0200

    ajout condition max 7 champs opening

--- public/js/addHoraires.js
+++ public/js/addHoraires.js
@@ -2,18 +2,27 @@
 // Définition du bouton pour ajouter des horaires
 var addOpeningButton = $('#add-opening');
 
+// Tableau de jours de la semaine
+var joursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
+// Index de chaque champs
+var indexHoraires = 0
+
 // Fonction qui se déclenche au clic du bouton
 $(addOpeningButton).click(function() {
-  
-var horairesCollectionHolder = document.querySelector('#barbershop_horaires');
 
+    if (indexHoraires <=6){
   
-console.log(horairesCollectionHolder.dataset.prototype)
+        var horairesCollectionHolder = document.querySelector('#barbershop_horaires');
 
-  
-horairesCollectionHolder.innerHTML += horairesCollectionHolder.dataset.prototype.replace(/__name__/g, indexHoraires);
-  
-indexHoraires++;
-console.log(indexHoraires)
-  
+        console.log(horairesCollectionHolder.dataset.prototype)
+        
+        horairesCollectionHolder.innerHTML += horairesCollectionHolder.dataset.prototype.replace(/__name__label__/g, joursSemaine[indexHoraires]);
+        
+        indexHoraires++;
+        console.log(indexHoraires)
+    }
+    else{
+        console.log('MAX');
+    }
  });
