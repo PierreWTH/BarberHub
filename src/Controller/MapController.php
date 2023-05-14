9d64884d9f7029afbfcbc92326ebb4768feded0f
PierreWTH <wietrichpierre@gmail.com>
2023-05-14 20:29:53 +0200

    leaflet fonctionnel

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Barbershop;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'app_map')]
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response
+    {   
+        $barbershops = $doctrine->getRepository(Barbershop::Class)->findBy([], ["nom"=>"ASC"]);
+        
+        foreach ($barbershops as $barbershop) {
+            $latitude = $barbershop->getLatitude();
+            $longitude = $barbershop->getLongitude();
+            
+            // Ajoutez les coordonnées à un tableau
+            $coordinates[] = [
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+            ];
+        }
+
         return $this->render('map/index.html.twig', [
-            'controller_name' => 'MapController',
+            'coordinates' => $coordinates
         ]);
     }
 }
