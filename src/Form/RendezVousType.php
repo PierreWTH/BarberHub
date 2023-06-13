3f74e91486863068ec5d16f02d956946ff4ad32f
PierreWTH <wietrichpierre@gmail.com>
2023-06-14 00:05:02 +0200

    rdv form en cours

--- src/Form/RendezVousType.php
+++ src/Form/RendezVousType.php
@@ -5,16 +5,27 @@ namespace App\Form;
 use App\Entity\RendezVous;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class RendezVousType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('debut')
-            ->add('fin')
+            ->add('debut', DateTimeType::class, [
+                'time_label' => 'Starts On',
+                'minutes' => range(0, 30, 30),
+                'attr' => [
+                    'min' => date('Y-m-d')
+                ]
+            ])
+            ->add('fin', DateTimeType::class, [
+                'time_label' => 'Starts On',
+                'minutes' => range(0, 30, 30),
+            ])
             ->add('barberPrestation')
             ->add('personnel')
             ->add('submit', SubmitType::class)
