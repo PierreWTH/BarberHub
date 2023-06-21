67da3a9dcc41d996ee2e050fd710c7c41e4a76d2
PierreWTH <wietrichpierre@gmail.com>
2023-06-21 23:28:08 +0200

    récuperation heureDebut + heureFin

--- src/Form/RendezVousType.php
+++ src/Form/RendezVousType.php
@@ -23,10 +23,7 @@ class RendezVousType extends AbstractType
 
         $builder
         
-            ->add('debut', ChoiceType::class, [
-                'choices' => $plagesHoraires,
-                'label' => false,
-            ])
+            ->add('debut', HiddenType::class)
 
             ->add('fin', HiddenType::class)
 
