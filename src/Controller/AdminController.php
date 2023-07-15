cff40960686546f57d854fbf6529a2f8b731c2be
PierreWTH <wietrichpierre@gmail.com>
2023-07-15 18:41:48 +0200

    edit user function & form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditUserType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,7 +31,7 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/administration/users', name: 'admin_barbershop')]
+    #[Route('/administration/users', name: 'admin_users')]
     #[IsGranted('ROLE_ADMIN')]
     public function listUsers(ManagerRegistry $doctrine): Response
     {
@@ -38,5 +40,26 @@ class AdminController extends AbstractController
         return $this->render('security/admin/users.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    #[Route('/administration/users/edit/{id}', name: 'admin_edituser')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function editUsers(User $user, Request $request): Response
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_utilisateurs');
+        }
+    
+    return $this->render('security/admin/edituser.html.twig', [
+        'userForm' => $form->createView(),
+    ]);
     }
 }
