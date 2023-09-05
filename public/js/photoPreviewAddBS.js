2a2e67f9a3153772fc28ca91b410b6626a336dc7
PierreWTH <wietrichpierre@gmail.com>
2023-09-05 09:24:07 +0200

    preview images multiples

--- public/js/photoPreviewAddBS.js
+++ public/js/photoPreviewAddBS.js
@@ -1,14 +1,22 @@
-var input = document.getElementById('barbershop_images');
-var preview = document.getElementById('image-preview');
+$(document).ready(function () {
+    var input = document.getElementById('barbershop_realisations');
+    var preview = $('#image-preview');
 
-input.addEventListener('change', function (e) {
-    var file = e.target.files[0]
-    var reader = new FileReader();
+    input.addEventListener('change', function (e) {
+        preview.empty(); // Efface le contenu précédent de la prévisualisation
 
-    reader.onload = function(e) {
-        preview.src = e.target.result;
-        preview.style.display = 'block';
-    };
+        for (var i = 0; i < e.target.files.length; i++) {
+            var file = e.target.files[i];
+            var reader = new FileReader();
 
-    reader.readAsDataURL(file);
-})
+            reader.onload = function (event) {
+                var img = $('<img>'); // Crée un élément img jQuery
+                img.attr('src', event.target.result);
+                img.css('max-width', '200px'); // Vous pouvez définir la largeur maximale de l'image
+                preview.append(img);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    });
+});
